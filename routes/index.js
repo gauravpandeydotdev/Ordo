2b26b722c0ad8e0c4d4620bba0cf0f79aeca51d9
gauravpandey <gaurav91pandey@gmail.com>
2022-04-11 10:36:06 -0700

    refactor according to Google JS style guide

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,13 @@ const ytdlpService = require('../services/ytdlpService');
 const config = require('../services/configParser');
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-    res.render('index', {
-        title: "PlexTube",
-        version: await ytdlpService.getYtdlpVersion(),
-        config: config.getConfig(),
-        envConfig: config.getEnvConfig()
-    });
+router.get('/', async function(req, res, next) {
+  res.render('index', {
+    title: 'PlexTube',
+    version: await ytdlpService.getYtdlpVersion(),
+    config: config.getConfig(),
+    envConfig: config.getEnvConfig(),
+  });
 });
 
 module.exports = router;

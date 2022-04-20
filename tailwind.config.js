f5e0f6cb1c4f39b3e1647b5033290354c295c17c
gauravpandey <gaurav91pandey@gmail.com>
2022-04-20 09:21:29 -0700

    added screens breakpoint in tailwind.config.js
    1) found a better way to disable errors on generated style.css by @tailwind base,
    2) re enabled @tailwind base.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ['./views/*.ejs'],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {},
   },
   plugins: [],
